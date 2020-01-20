d2dc9956948669c297118392a0c06dd3e5f2ee8b
Gabriel Pinheiro <pinheiro.gbr@gmail.com>
2020-01-20 00:06:12 -0300

    currencies working

--- lib/currency.ex
+++ lib/currency.ex
@@ -1,3 +1,26 @@
 defmodule FinancialSystem.Currency do
+  @moduledoc """
+    Currency module works to get all the currency values using the exangeratesapi where the base money is EUR.
+    It follows the ISO 4217
+  """
+  use Tesla
 
+  plug Tesla.Middleware.BaseUrl, "https://api.exchangeratesapi.io"
+  plug Tesla.Middleware.JSON
+
+  def get_currencies() do
+    # middleware = [
+    #   {
+    #     Tesla.Middleware.BaseUrl, "https://api.exchangeratesapi.io",
+    #     Tesla.Middleware.JSON
+    #   }
+    # ]
+    # client = Tesla.client(middleware)
+    case get(
+      "/latest"
+    ) do {:ok, %Tesla.Env{status: 200, body: body}} ->
+      body["rates"]
+      # File.write("currencies.json", body)
+    end
+  end
 end
